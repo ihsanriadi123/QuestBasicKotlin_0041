7c1269f2c79e7928dd8f25a8da73758e07589a91
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-10-11 22:06:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/TipeData.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+fun main(){
+
+    //Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //long
+    val longValue: Long = 48
+    println("Long: $longValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    //Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    //Boolean
+    val  booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //string
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hellow, kotlin"
+    println("string: $stringValue")
+}
