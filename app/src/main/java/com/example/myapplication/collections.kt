e41cedf7969c4143fd0345e09d83d12ab40a1139
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-10-12 10:32:59 +0700

    Menambahkan penanganan list yang mutable dan read-only

--- app/src/main/java/com/example/myapplication/collections.kt
+++ app/src/main/java/com/example/myapplication/collections.kt
@@ -33,6 +33,8 @@ fun ContohList() {
     println(shapesLocked)
 }
 
+//Menambahkan penanganan list yang mutable dan read-only
+
 // Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik (tidak ada duplikasi)
 // Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
 // Set bersifat mutable dan read-only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
